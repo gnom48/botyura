21be131404ec18d6ce9fb2f129e7c0c2fdbcafbd
Darik097 <150348428+Darik097@users.noreply.github.com>
2023-11-12 23:06:23 +0300

    Update inline_keybords.py

--- keybords/inline_keybords.py
+++ keybords/inline_keybords.py
@@ -30,3 +30,38 @@ def get_realtors_type_kb() -> InlineKeyboardMarkup:
     inline_kb_realtors_type = InlineKeyboardMarkup(row_width=1)
     inline_kb_realtors_type.add(InlineKeyboardButton(text='Риелтор жилой недвижимости', callback_data='residential'), InlineKeyboardButton(text='Риелтор комерческой недвижимости', callback_data='commercial'))
     return inline_kb_realtors_type
+
+
+def get_inline_menu_markup() -> InlineKeyboardMarkup:
+    buttons = [
+        InlineKeyboardButton("Аналитика", callback_data="analytics"),
+        InlineKeyboardButton("Встреча", callback_data="meeting"),
+        InlineKeyboardButton("Обзвон", callback_data="call"),
+        InlineKeyboardButton("Показ", callback_data="show"),
+        InlineKeyboardButton("Поиск", callback_data="search"),
+        InlineKeyboardButton("Расклейка", callback_data="flyer"),
+        InlineKeyboardButton("Сделка", callback_data="deal"),
+        InlineKeyboardButton("Задаток", callback_data="deposit"),
+        InlineKeyboardButton("Отпуск", callback_data="vacation"),
+        InlineKeyboardButton("Больничный", callback_data="sick_leave"),
+    ]
+
+    inline_markup = InlineKeyboardMarkup(row_width=2)
+    inline_markup.add(*buttons)
+
+    return inline_markup
+
+
+def get_inline_meeting_markup() -> InlineKeyboardMarkup:
+    buttons = [
+        InlineKeyboardButton("Квартира", callback_data="apartment"),
+        InlineKeyboardButton("Земля", callback_data="lend"),
+        InlineKeyboardButton("Дом", callback_data="house"),
+        InlineKeyboardButton("Назад", callback_data="bsck"),
+    ]
+
+    inline_markup = InlineKeyboardMarkup(row_width=2)
+    inline_markup.add(*buttons)
+
+    return inline_markup
+
